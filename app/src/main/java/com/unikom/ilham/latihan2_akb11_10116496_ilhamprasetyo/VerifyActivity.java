b32440f690c50139a108e9b5d5cecb2fcbcae9b3
Ilham Prasetyo <Ilprasetyo2125@gmail.com>
2019-04-18 21:42:12 +0700

    identitas

--- app/src/main/java/com/unikom/ilham/latihan2_akb11_10116496_ilhamprasetyo/VerifyActivity.java
+++ app/src/main/java/com/unikom/ilham/latihan2_akb11_10116496_ilhamprasetyo/VerifyActivity.java
@@ -1,5 +1,10 @@
 package com.unikom.ilham.latihan2_akb11_10116496_ilhamprasetyo;
 
+// Nama           : Ilham Prasetyo
+// NIM            : 10116496
+// Kelas          : AKB-11
+// Tgl Pengerjaan : 18 April 2019
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
